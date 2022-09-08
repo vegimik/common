2420fa82f1632c5cfbd464866cdc7b704a99b9e9
Vegim Karaçica <vegimi.k@hotmail.com>
2022-09-08 15:01:22 +0200

    Updates

---
+++ src/events/_ticketCreatedEvent.ts
@@ -0,0 +1,11 @@
+import { Subjects } from "./_subject";
+
+export default interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
---
+++ src/events/_ticketUpdatedEvent.ts
@@ -0,0 +1,11 @@
+import { Subjects } from "./_subject";
+
+export default interface TicketUpdatedEvent {
+  subject: Subjects.TickerUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
