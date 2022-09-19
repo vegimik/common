7ad4c982d74cffaab5b79f284eb4775f0345f40a
Vegim Karaçica <vegimi.k@hotmail.com>
2022-09-19 17:07:21 +0200

    Updates (version: 1.0.23)

--- src/events/_ticketCreatedEvent.ts
+++ src/events/_ticketCreatedEvent.ts
@@ -4,9 +4,9 @@ export default interface TicketCreatedEvent {
   subject: Subjects.TicketCreated;
   data: {
     id: string;
-    version: number;
     title: string;
     price: number;
     userId: string;
+    version: number;
   };
 }
--- src/events/_ticketUpdatedEvent.ts
+++ src/events/_ticketUpdatedEvent.ts
@@ -4,9 +4,9 @@ export default interface TicketUpdatedEvent {
   subject: Subjects.TickerUpdated;
   data: {
     id: string;
-    version: number;
     title: string;
     price: number;
     userId: string;
+    version: number;
   };
 }
